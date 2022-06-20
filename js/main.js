e75dc58e9390b40703927b38f5e22dc4186b3afe
Abhishek Kumar <akbittu@gmail.com>
2022-06-20 09:08:31 +0530

    Created pino log reader

---
+++ js/main.js
@@ -0,0 +1,178 @@
+/** 
+ * @author Abhishek Kumar
+ * @license This work is licensed under a MIT License.
+ */
+
+const LOG_LEVELS = {
+  80: 'emerg',
+  70: 'alert',
+  60: 'crit',
+  50: 'error',
+  40: 'warn',
+  30: 'notice',
+  20: 'info',
+  10: 'debug',
+};
+
+var main = function(e) {
+    console.log('main');
+
+    var LogReader = function() {
+        var containers = {},
+            controls = {},
+            model = {};
+        var getParsedData = function(data) {
+            var ilines = data.split('\n'),
+                olines = [];
+            for (let i = 0; i < ilines.length; i++) {
+                let line = (!!ilines[i]) ? JSON.parse(ilines[i]) : ilines[i];
+                if (i == 0) {
+                    let titles = Object.keys(line).join('</th><th>');
+                    olines.push('<thead><tr><th>' + titles + '</th></tr></thead>');
+                }
+                line.level = LOG_LEVELS[line.level];
+                line.time = (!!line.time) ? line.time.replace('T', ' ').replace('Z', '') : line.time;
+                let cells = Object.values(line).join('</td><td>');
+                olines.push('<td>' + cells + '</td>');
+            }
+            let output = '<tbody><tr>' + olines.join('</tr><tr>') + '</tr></tbody>';
+            return '<table>' + output + '</table>';
+        }
+        var getFilteredData = function(pattern, data) {
+            var output = data;
+            if (!!pattern) {
+                var ilines = data.split('\n'),
+                    olines = [];
+                for (let i = 0; i < ilines.length; i++) {
+                    if (ilines[i].indexOf(pattern) >= 0) {
+                        olines.push(ilines[i]);
+                    }
+                }
+                output = olines.join('\n');
+            }
+            return output;
+        }
+        var renderOutput = function(data) {
+            if (!!data) {
+                let filteredData = getFilteredData(containers.filterStr.val(), data);
+                containers.outputResponse.html(filteredData);
+                let parsedData = getParsedData(filteredData);
+                containers.outputParsed.html(parsedData);
+            }
+        }
+        var readQueryParam = function () {
+            model.urlQueryParams = new URLSearchParams(window.location.search);
+        }
+        var loadRemoteFile = function(link) {
+            let url = decodeURIComponent(link);
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    model.cachedFileContent = data;
+                    renderOutput(model.cachedFileContent);
+                    containers.output.removeClass('hide');
+                });
+        }
+
+        controls.handleFileSelect = function(evt) {
+            console.log('controls.handleFileSelect', evt);
+            let files = evt.target.files;
+            if (files.length > 0) {
+                var fileExtension = /\.(log|txt)$/gi;
+                var fileTobeRead = files[0];
+                if (fileTobeRead.name.match(fileExtension)) {
+                    var fileReader = new FileReader();
+                    fileReader.onload = function(e) {
+                        model.cachedFileContent = fileReader.result;
+                        renderOutput(model.cachedFileContent);
+                    }
+                    fileReader.readAsText(fileTobeRead);
+                    containers.output.removeClass('hide');
+                } else {
+                    alert("Please select log file");
+                }
+            }
+        }
+        controls.handleOutputFormat = function(e) {
+            console.log('controls.handleOutputFormat', e);
+            let outputFmt = e.target.value;
+            if (outputFmt == "raw") {
+                containers.outputRaw.removeClass('hide');
+                containers.outputParsed.addClass('hide');
+            } else {
+                containers.outputRaw.addClass('hide');
+                containers.outputParsed.removeClass('hide');
+            }
+        }
+        controls.rerenderOutput = function(e) {
+            e.preventDefault();
+            renderOutput(model.cachedFileContent);
+        }
+        controls.noAction = function(e) {
+            e.preventDefault();
+        }
+        controls.resetAtDefaultState = function(e) {
+            containers.outputResponse.html('');
+            containers.outputParsed.html('');
+            containers.output.addClass('hide');
+            containers.fmtRaw.trigger('click');
+            return
+        }
+        controls.showAbout = function(e) {
+            containers.appAbout.removeClass('hide');
+        }
+        controls.hideAbout = function(e) {
+            containers.appAbout.addClass('hide');
+        }
+
+        this.destroy = function() {
+            console.log('LogReader', cid, 'destroy');
+            containers.fileRef.off('change', controls.handleFileSelect);
+            containers.fmtRaw.off('click', controls.handleOutputFormat);
+            containers.fmtParsed.off('click', controls.handleOutputFormat);
+        }
+        this.initialize = function() {
+            console.log('LogReader initialized');
+            containers.appLogo = $('.app-logo');
+
+            containers.appAbout = $('#appAbout');
+            containers.appAboutClose = containers.appAbout.find('.modal-btn-close');
+
+            containers.output = $('.output');
+            containers.outputRaw = containers.output.find('pre');
+            containers.outputResponse = containers.output.find('code');
+            containers.outputParsed = containers.output.find('.parsed');
+
+            containers.ctrlPanel = $('form#ctrlPanel');
+            containers.filterStr = $('input#filterStr');
+            containers.fmtRaw = $('input#fmtRaw');
+            containers.fmtParsed = $('input#fmtParsed');
+            containers.fileRef = $('input#fileRef');
+            containers.btnReset = $('input#btnReset');
+
+            readQueryParam();
+            if (!!model.urlQueryParams.get('link')) {
+                containers.fileRef.addClass('hide');
+                containers.btnReset.addClass('hide');
+                loadRemoteFile(model.urlQueryParams.get('link'));
+            } else {
+                containers.fileRef.on('change', controls.handleFileSelect);
+            }
+            containers.fmtRaw.on('click', controls.handleOutputFormat);
+            containers.fmtParsed.on('click', controls.handleOutputFormat);
+            containers.ctrlPanel.on('reset', controls.resetAtDefaultState);
+            containers.ctrlPanel.on('submit', controls.rerenderOutput);
+            containers.filterStr.on('blur', controls.rerenderOutput);
+
+            containers.appLogo.on('click', controls.showAbout);
+            containers.appAboutClose.on('click', controls.hideAbout);
+
+            containers.ctrlPanel.trigger('click');
+        }
+    }
+
+    var app = new LogReader();
+    app.initialize();
+};
+
+$(main);
